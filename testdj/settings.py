a14f0ab95e2725f7161f0d38b25e9ea366d98ac2
nickolae96 <nickolae.enachi@yandex.com>
2022-06-02 12:13:53 +0300

    fixed settings.py

--- testdj/settings.py
+++ testdj/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'testdj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'demo_1',
-        'USER': 'nickolae',
-        'PASSWORD': 'qazwsx321',
-        'HOST': 'database-1.c2wliobissle.us-east-1.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR.joinpath('db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'demo_1',
+#         'USER': 'nickolae',
+#         'PASSWORD': 'qazwsx321',
+#         'HOST': 'database-1.c2wliobissle.us-east-1.rds.amazonaws.com',
+#         'PORT': '5432'
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.joinpath('db.sqlite3'),
+    }
+}
 
 
 # Password validation
